a917e9a699f9fbf141de102bb9afb6ac16b7f2e3
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-11-19 10:14:39 +0800

    Add files via upload

---
+++ Scanner_1.java
@@ -0,0 +1,29 @@
+package scanner_1;
+import java.util.Scanner;
+public class Scanner_1 {
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+String nama,umur,tinggibadan,kelas,beratbadan,asal,
+nim,prodi;
+char gendeer;
+    System.out.print("Nama = ");
+    nama = sc.nextLine();
+    System.out.print("Nim = ");
+    nim = sc.nextLine();
+    System.out.print("Kelas = ");
+    kelas = sc.nextLine();
+    System.out.print("Prodi = ");
+    prodi = sc.nextLine();
+    System.out.print("Asal = ");
+    asal = sc.nextLine();
+    System.out.print("Tinggi Badan = ");
+    tinggibadan = sc.nextLine();
+    System.out.print("Berat Badan = ");
+    beratbadan = sc.nextLine();
+    System.out.print("Gender = ");
+    char gender = (char) sc.nextLong();
+    System.out.println("");
+      
+    }
+    
+}
