fb8c6d3aaa12a4ea2e762f6c87ff41b492fd528d
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-11-19 10:49:28 +0800

    Add files via upload

---
+++ Kode_baca_file_3.java
@@ -0,0 +1,17 @@
+package kode_baca_file_3;
+public class Kode_baca_file_3 {
+    public static void main(String[] args) {
+         String nama = "Riwandi Kusuma Wijaya";
+      String nim = "D0223323";
+      String alamat = "Lembang";
+      double tinggiBadan = 1.5;
+      int beratBadan = 50;
+        System.out.println("Nama = " + nama);
+        System.out.println("Nim = " + nim);
+        System.out.println("Alamat = " + alamat);
+        System.out.println("Tinggi Badan = " + tinggiBadan + "m");
+        System.out.println("Berat Badan = " + beratBadan + " kg");
+        
+    }
+    
+}
